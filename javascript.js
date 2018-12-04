90b6d225e3c10f48c43744575c7f33795e82b81b
OthiDaniel <othidaniel@gmail.com>
2018-12-04 14:33:24 +0300

    adjusting to errors

--- javascript.js
+++ javascript.js
@@ -2,18 +2,19 @@ function tracker() {
   var a=parseInt(getElementById('a').value);
   var b=parseInt(getElementById('b').value);
   var c=parseInt(getElementById('c').value);
-  var return=document.getElementById('return');
+  var view=document.getElementById('return');
 
 
   if(A + B > C && A + C > B && B + C > A){
     if (A == B && B == C){
-      return.innerHTML='Equilateral Triangle';
+      view.innerHTML='Equilateral Triangle';
     }
     else if(A == B || B == C || A == C){
-      return.innerHTML='Isosceles Triangle';
+      view.innerHTML='Isosceles Triangle';
     }
-    else(){
-      return.innerHTML="Scalene Triangle"
+    else{
+      view.innerHTML="Scalene Triangle"
     }
   }
+  else if
 }
